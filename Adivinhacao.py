c9ad4556cadea98cd3bb611eaa71360ba47cb7fd
Victor Fondello <fondello92@gmail.com>
2023-11-05 11:04:30 +0100

    Jogo numero aleatorio em Python

---
+++ Adivinhacao.py
@@ -0,0 +1,25 @@
+import random
+
+print("Bem-vindo ao jogo de adivinhação em Python!\n")
+
+numero_maximo = 50
+numero_aleatorio = random.randint(1, numero_maximo)
+quantidade_tentativas = 1
+
+print("Digite um número entre 1 e", numero_maximo, "\n")
+
+chute = input("Digite o número desejado: ")
+chute_convertido = int(chute)
+
+while chute_convertido != numero_aleatorio:
+
+    if chute_convertido < numero_aleatorio:
+        print("O número secreto é maior!")
+    else:
+        print("O número secreto é menor!")
+
+    chute = input("Digite o número desejado: ")
+    chute_convertido = int(chute)
+    quantidade_tentativas += 1
+
+print("Parabéns, você acertou o número secreto com", quantidade_tentativas, "tentativas")
